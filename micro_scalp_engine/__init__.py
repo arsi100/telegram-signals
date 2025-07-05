69b6b11179210f9ada6565e8b1bea7496e1a8bab
arsi100 <arsi.siddiqi@gmail.com>
2025-07-05 17:36:27 -0400

    Fix: Already disabled backtest_visualizer import

--- micro_scalp_engine/__init__.py
+++ micro_scalp_engine/__init__.py
@@ -3,19 +3,29 @@ MICRO-SCALP engine package.
 """
 
 # from . import backtester  # Temporarily disabled due to syntax errors
-from . import backtest_visualizer
+# from . import backtest_visualizer  # Temporarily disabled - requires seaborn
+from . import data_ingestion
+from . import data_processor
 from . import entry_logic
 from . import level_finder
 from . import logic_engine
 from . import position_tracker
+from . import pattern_recognition
+from . import risk_management
+from . import order_execution
 
 __all__ = [
-    # 'backtester',  # Temporarily disabled due to syntax errors
-    'backtest_visualizer',
+    'data_ingestion',
+    'data_processor',
+    'logic_engine',
+    # 'backtester',  # Temporarily disabled
+    # 'backtest_visualizer',  # Temporarily disabled
     'entry_logic',
     'level_finder',
-    'logic_engine',
-    'position_tracker'
+    'position_tracker',
+    'pattern_recognition',
+    'risk_management',
+    'order_execution'
 ]
 
 """
